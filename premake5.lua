b44f4533a3907cf866616f6a7b170f64a4db3715
astone3080 <astone3080@naver.com>
2021-01-08 13:15:14 +0900

    add premake

---
+++ premake5.lua
@@ -0,0 +1,105 @@
+workspace "GameEngine"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "GameEngine"
+	location "GameEngine"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir("bin/" ..outputdir.. "/%{prj.name}")
+	objdir("bin-int/" ..outputdir.. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"ENGINE_PLATFORM_WINDOWS",
+			"ENGINE_BUILD_DLL"
+		}
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildcfg.relpath} .../bin/" ..outputdir.. "/Game")
+		}
+	filter "configurations:Debug"
+		defines	"EGINE_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "ENGINE_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "ENGINE_DIST"
+		optimize "On"
+
+project "Game"
+	location "Game"
+	kind "ConsoleApp"
+
+	language "C++"
+
+	targetdir ("bin/".. outputdir.."/%{prj.name}")
+	objdir ("bin-int/".. outputdir.."/%{prj.name}")
+	
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/vendor/spdlog/include",
+		"GameEngine/src"
+	}
+
+	links
+	{
+		"GameEngine"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"ENGINE_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		defines	"EGINE_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "ENGINE_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "ENGINE_DIST"
+		optimize "On"
