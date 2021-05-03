fed23eaaeae911663c2ee141994a49473bd6d32b
gv8b00ff <gv8b00ff@gmail.com>
2021-05-03 20:39:50 +0900

    Update UIComponents.cpp

--- Popeye/src/Popeye/Component/UIComponents.cpp
+++ Popeye/src/Popeye/Component/UIComponents.cpp
@@ -1,19 +1,1 @@
 #include "UIComponents.h"
-#include <iostream>
-
-using namespace std;
-{
-  private:
-    int xpos;
-    int ypos;
-
-public:
-  void PointSet(int x, int y);
-  void ShowPosition();
-};
-
-void Point::PointSet(int x, int y)
-{
-  xpos=x;
-  ypos=y;
-}
